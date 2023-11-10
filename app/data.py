acacef920aa83f2d6861f0a9fc2f58a3c2cfcb1a
denver-edwards <csdedwards@gmail.com>
2023-11-10 15:57:04 -0500

    sprint1
    database support

--- app/data.py
+++ app/data.py
@@ -8,18 +8,48 @@ from pymongo import MongoClient
 
 
 class Database:
+    # Load environment variables and connect to the database
+    load_dotenv()
+    DB_URL = getenv("DB_URL")
+    db = MongoClient(DB_URL, tlsCAFile=where())["Database"]
+
+    def __init__(self):
+        self.collection = self.db["collection"]
+
+        # If the collection is empty, seed it with initial data
+        if self.count() == 0:
+            self.seed(2048)
 
     def seed(self, amount):
-        pass
+        monster_list = []
+
+        for num in range(amount):
+            monster_list.append(Monster().to_dict())
+
+        self.collection.insert_many(monster_list)
 
     def reset(self):
-        pass
+        # Delete all documents in the collection
+        self.collection.delete_many({})
 
     def count(self) -> int:
-        pass
+        # Return the number of documents in the collection
+        return self.collection.count_documents({})
 
     def dataframe(self) -> DataFrame:
-        pass
+        # Convert the collection documents to a pandas DataFrame
+        docs = self.collection.find({})
+        return DataFrame(list(docs))
 
     def html_table(self) -> str:
-        pass
+        # Generate an HTML table representation of the collection data
+        if self.count() == 0:
+            return None  # Return None if the collection is empty
+        else:
+            return self.dataframe().to_html(index=True, header=True,
+                                            columns=["Name", "Type",
+                                                     "Level", "Rarity",
+                                                     'Damage', 'Health',
+                                                     "Energy",
+                                                     "Sanity",
+                                                     "Timestamp"])
